8d8cdfd21a826f8db3a27f4a9b50a620777b2946
Dgibson89 <donovan.gibson89@gmail.com>
2023-12-05 20:47:56 -0600

    DisplayCards.js Gave the cards a dark theme and added a link to a wikipedia page for each location listed through props.

--- src/components/DisplayCards.js
+++ src/components/DisplayCards.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card'; 
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 export default function DisplayCards(props) {
-    return (
-        <Card style={{ width: '18rem' }} className='mb-4'>
-          <Card.Img variant="top" src={`./${props.image}`} />
-          <Card.Body>
-            <Card.Title>{props.title}</Card.Title>
-            <Card.Text>
-            {props.description}
-            </Card.Text>
-            <Button variant="primary">More Information</Button>
-          </Card.Body>
-        </Card>
-      );
+  return (
+    <Card style={{ width: "18rem" }} data-bs-theme="dark" className="mb-4">
+      <Card.Img variant="top" src={`./${props.image}`} className="card--img" />
+      <Card.Body>
+        <Card.Title>{props.title}</Card.Title>
+        <Card.Text>{props.description}</Card.Text>
+        <Button variant="light" href={props.wikilink} target="_blank">
+          More Information
+        </Button>
+      </Card.Body>
+    </Card>
+  );
 }
