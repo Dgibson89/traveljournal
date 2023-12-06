36e0573c4c20fcbe6d7e1681499c1cd7fe33ffa6
Dgibson89 <donovan.gibson89@gmail.com>
2023-12-05 20:49:32 -0600

    Navbar.js Visually not much has changed but it is now using bootstraps Navbar component.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
 
 export default function () {
   return (
-    <div className="nav--container">
-      <img className="nav--img" src="./ghost.webp" />
-      <p className="nav--title">my future travel journal</p>
-    </div>
+    <Navbar expand="lg" data-bs-theme="dark" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home" className="nav--title">
+          <img className="nav--img" src="./ghost.webp" />
+          my future travel journal
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
   );
 }
