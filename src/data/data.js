7dc434f3860290654215c7c7be3c0e8d89bc57f9
Dgibson89 <donovan.gibson89@gmail.com>
2023-12-05 20:50:48 -0600

    data.js The start and end date data was removed and replaced by the wiki link to be passed to each cards button.

--- src/data/data.js
+++ src/data/data.js
@@ -3,8 +3,7 @@ export default [
     title: "Aokigahara Forest",
     location: "Japan",
     googleMapsUrl: "https://www.google.com/maps/place/Aokigahara+Forest",
-    startDate: "TBD",
-    endDate: "TBD",
+    wikilink: "https://en.wikipedia.org/wiki/Aokigahara",
     description:
       "Aokigahara, also known as the Sea of Trees, is a forest on the northwestern flank of Mount Fuji thriving on 30 square kilometres of hardened lava laid down by the last major eruption of Mount Fuji in 864 CE. The forest has a historical reputation as a home to yūrei: ghosts of the dead in Japanese mythology.",
     image: "aokigaharaforest.jpg",
@@ -14,8 +13,7 @@ export default [
     title: "Hoia-Baciu Forest",
     location: "Romania",
     googleMapsUrl: "https://www.google.com/maps/place/Hoia+Forest",
-    startDate: "TBD",
-    endDate: "TBD",
+    wikilink: "https://en.wikipedia.org/wiki/Hoia_Forest",
     description:
       "Warped trees fill this forest, their skeletal figures twisting and spiraling, making it seem as though they’re contorting themselves to reach out and touch you. An eerie silence fills the air, interrupted only by the footsteps of unseen figures.",
     image: "hoiabaciuforest.jpg",
@@ -25,8 +23,7 @@ export default [
     title: "Catacombes de Paris",
     location: "France",
     googleMapsUrl: "https://www.google.com/maps/place/Catacombs+of+Paris",
-    startDate: "TBD",
-    endDate: "TBD",
+    wikilink: "https://en.wikipedia.org/wiki/Catacombs_of_Paris",
     description:
       "The Catacombs of Paris are underground ossuaries in Paris, France, which hold the remains of more than six million people. As one visits the catacombs, a sign above reads Arrête! C'est ici l'empire de la Mort ('Stop! This is the empire of Death').",
     image: "pariscatacombs.jpg",
@@ -36,8 +33,7 @@ export default [
     title: "The Stanley Hotel",
     location: "Colorado",
     googleMapsUrl: "https://www.google.com/maps/place/Stanley+Hotel",
-    startDate: "TBD",
-    endDate: "TBD",
+    wikilink: "https://en.wikipedia.org/wiki/The_Stanley_Hotel",
     description:
       "This famous Rocky Mountain destination is known as one of the inspirations for Stephen King's 'The Shining,' but it also has its own spooky past.",
     image: "stanleyhotel.jpg",
